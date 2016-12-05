2eb2887933c885d700c7d97ca9312c81f898b387
quhuainan <1026323214@qq.com>
2016-12-05 10:11:03 +0800

    初识动画

--- app/src/main/java/com/qhn/bhne/androiddevelopertrainsample/MainActivity.java
+++ app/src/main/java/com/qhn/bhne/androiddevelopertrainsample/MainActivity.java
@@ -1,7 +1,11 @@
 package com.qhn.bhne.androiddevelopertrainsample;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.btn_send_text)
+    public void onClick() {
+        Intent ine=new Intent();
+        ine.setAction(Intent.ACTION_SEND);
+        ine.putExtra(Intent.EXTRA_TEXT,"第一次分享数据");
+        ine.setType("text/plain");
+        //startActivity(ine);
+
+        startActivity(Intent.createChooser(ine,"哈哈"));
     }
 }
